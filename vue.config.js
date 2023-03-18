abcd8ce8e131fb90f05088256dc1b947d7d19fa8
asads30 <itmasteruz@yandex.com>
2023-03-18 13:54:04 +0500

    three

--- vue.config.js
+++ vue.config.js
@@ -30,6 +30,7 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
+    proxy: 'https://wpaka.site/',
     port: port,
     open: true,
     overlay: {
