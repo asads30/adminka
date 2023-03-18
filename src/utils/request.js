1e2a5a6e268aaa8aa50ce1a099c372ada9e36088
asads30 <itmasteruz@yandex.com>
2023-03-18 13:52:45 +0500

    two

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,16 +1,11 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
-import { authDate, firstName, userId, username } from '@/utils/auth'
 
 const service = axios.create({
   baseURL: '/admin/',
   withCredentials: true,
   timeout: 5000
 })
-
-const test = `auth_date=${authDate}\nfirst_name=${firstName}\nid=${userId}\nusername=${username}`
-
-console.log(test)
 
 service.interceptors.response.use(
   response => {
