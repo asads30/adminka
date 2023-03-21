c4136741bc08ee3b86ed3663d518f55147218e31
asads30 <itmasteruz@yandex.com>
2023-03-21 20:09:45 +0500

    admin2

--- src/permission.js
+++ src/permission.js
@@ -2,13 +2,20 @@ import router from './router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import getPageTitle from '@/utils/get-page-title'
+import { getTg } from '@/utils/auth'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 router.beforeEach(async(to, from, next) => {
   NProgress.start()
   document.title = getPageTitle(to.meta.title)
-  next()
+  var telegram = getTg()
+  if (telegram) {
+    next()
+  } else {
+    next(`/login?redirect=${to.path}`)
+    NProgress.done()
+  }
 })
 
 router.afterEach(() => {
